2f058dec52a8326f6b8ca2eae65335818fa09772
Juan Pablo Genovese <juanpgenovese@gmail.com>
2015-06-11 19:17:00 -0300

    Add type declaration

--- writing-web-apps/gowiki/wiki.go
+++ writing-web-apps/gowiki/wiki.go
@@ -4,3 +4,10 @@ import (
 	"fmt"
 	"io/ioutil"
 )
+
+type Page struct {
+    // Title is a regular string
+    Title string
+    // Body is a "byte slice". They are like arrays, but way more flexible: http://blog.golang.org/go-slices-usage-and-internals
+    Body  []byte
+}
