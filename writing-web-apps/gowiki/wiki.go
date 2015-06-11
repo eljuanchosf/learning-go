fd0160817a760af8f977894c5a8d8e360112aabc
Juan Pablo Genovese <juanpgenovese@gmail.com>
2015-06-11 19:22:42 -0300

    Add load/save to disk methods

--- writing-web-apps/gowiki/wiki.go
+++ writing-web-apps/gowiki/wiki.go
@@ -11,3 +11,19 @@ type Page struct {
     // Body is a "byte slice". They are like arrays, but way more flexible: http://blog.golang.org/go-slices-usage-and-internals
     Body  []byte
 }
+
+// This method will save the above defined struct to disk
+func (p *Page) save() error {
+    filename := p.Title + ".txt"
+    return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+// This method will return a Page (defined above) by reading the data from a file in disk.
+func loadPage(title string) *Page {
+    filename := title + ".txt"
+    body, err := ioutil.ReadFile(filename)
+    if err != nil {
+        return nil, err
+    }
+    return &Page{Title: title, Body: body}
+}
