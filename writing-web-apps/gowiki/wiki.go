0a2285a590554f3ec67fe296f8135b5ea9c3ff50
Juan Pablo Genovese <juanpgenovese@gmail.com>
2015-06-11 19:30:09 -0300

    Add main methdod

--- writing-web-apps/gowiki/wiki.go
+++ writing-web-apps/gowiki/wiki.go
@@ -19,11 +19,19 @@ func (p *Page) save() error {
 }
 
 // This method will return a Page (defined above) by reading the data from a file in disk.
-func loadPage(title string) *Page {
+func loadPage(title string) (*Page, error) {
     filename := title + ".txt"
     body, err := ioutil.ReadFile(filename)
     if err != nil {
         return nil, err
     }
-    return &Page{Title: title, Body: body}
+    return &Page{Title: title, Body: body}, nil
+}
+
+// This is the main entry point to the program. Shoud print "This is a sample Page."
+func main() {
+    p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
+    p1.save()
+    p2, _ := loadPage("TestPage")
+    fmt.Println(string(p2.Body))
 }
